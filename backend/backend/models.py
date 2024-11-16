e741cfcb3b0a18607a3e626be8a892034dd9cb30
Karol-Trawinski <123karol.trawinski@gmail.com>
2024-11-16 17:59:29 +0100

    Create image model

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+class Image(models.Model):
+    name = models.CharField(max_length=255)
+    date = models.DateField(auto_now_add=True)
+    path = models.ImageField(upload_to='images/')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
